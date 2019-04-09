15da014e5f6fa51e6364a57c1dbc104b2df133be
Leonardo <leonardo@quetzal.com.br>
2019-04-09 12:33:20 -0300

    Add project structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform modelPrefab;
+
+    TransformAccessArray transforms;
+    MovementJob moveJob;
+    JobHandle moveHandle;
+
+    private void OnDisable() 
+    {
+        moveHandle.Complete();
+        transforms.Dispose();
+    }
+
+    void Start()
+    {
+        transforms = new TransformAccessArray(0, -1);
+    }
+
+    void Update()
+    {
+        moveHandle.Complete();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AddDecoys(100);
+        }
+
+        moveJob = new MovementJob()
+        {
+            moveSpeed = 1.0f,
+            bound1 = 20f,
+            bound2 = -20f,
+            deltaTime = Time.deltaTime
+        };
+        moveHandle = moveJob.Schedule(transforms);
+        JobHandle.ScheduleBatchedJobs();
+    }
+
+    private void AddDecoys(int amount)
+    {
+        moveHandle.Complete();
+        transforms.capacity = transforms.length + amount;
+        for(int i = 0; i < amount; i++)
+        {
+            float xVal = Random.Range(-20, 20);
+            float zVal = Random.Range(-20, 20);
+            Vector3 pos = new Vector3(xVal, 0f, zVal);
+            Quaternion rot = Quaternion.Euler(0f, 180f, 0f);
+            var obj = Instantiate(modelPrefab, pos, rot);
+            transforms.Add(obj.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/MovementJob.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public struct MovementJob : IJobParallelForTransform
+{
+    public float moveSpeed;
+    public float bound1;
+    public float bound2;
+    public float deltaTime;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        Vector3 pos = transform.position;
+        pos += moveSpeed * deltaTime * (transform.rotation * new Vector3(0f, 0f, 1f));
+        if(pos.z  < bound1)
+        {
+            pos.z = bound2;
+        }
+        transform.position = pos;
+    }
+}
